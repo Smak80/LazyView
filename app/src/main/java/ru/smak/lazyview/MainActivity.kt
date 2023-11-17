be8aef36cced4a893c6feb1821d86bff749d1cd7
Sergey Makletsov <smaklets@kpfu.ru>
2023-11-17 15:24:10 +0300

    Some corrections

--- app/src/main/java/ru/smak/lazyview/MainActivity.kt
+++ app/src/main/java/ru/smak/lazyview/MainActivity.kt
@@ -6,15 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -31,9 +37,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -57,7 +70,9 @@ class MainActivity : ComponentActivity() {
                 ){
                     LazyR(
                         lst.reversed(),
-                        Modifier.fillMaxWidth()
+                        Modifier
+                            .wrapContentHeight()
+                            .fillMaxWidth()
                     )
                     LazyC(
                         Modifier
@@ -134,8 +149,8 @@ fun LazyC(
                     .fillMaxWidth(),
                 fontSize = 14.sp,
                 fontWeight = FontWeight.Bold,
-
-                )
+                textAlign = TextAlign.Center,
+            )
         }
         items(lst){
             ElevatedCard(
@@ -165,27 +180,21 @@ fun LazyR(
     lst: List<String>,
     modifier: Modifier = Modifier,
 ){
-    Row(modifier = modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text = stringResource(R.string.lazy_row),
+            modifier = Modifier
+                .padding(8.dp),
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+        )
         LazyRow(
-            modifier = Modifier.height(100.dp),
+            modifier = Modifier.padding(vertical = 8.dp),
             horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            item {
-                Column(
-                    modifier = Modifier
-                        .rotate(-90f)
-                        .width(100.dp)
-                ) {
-                    Text(
-                        text = "Ленивая\nстрока:",
-                        modifier = Modifier
-                            .padding(2.dp)
-                            .fillMaxWidth(),
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Bold,
-                    )
-                }
-            }
             items(lst) {
                 ElevatedCard(
                     modifier = Modifier.fillMaxWidth(),
